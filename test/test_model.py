e2ca381474289668a27d304a801ed162df5738e6
Gustav Rasmussen <gcr84@hotmail.com>
2022-04-21 18:25:35 +0200

    test: add get_data test for the model module and refactor test_model.py

--- test/test_model.py
+++ test/test_model.py
@@ -11,7 +11,7 @@ import os
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
-from src.model.model import get_df, one_rep_max_estimator
+from src.model.model import get_df, one_rep_max_estimator, get_data
 
 from tinydb import TinyDB
 
@@ -28,9 +28,6 @@ table = (
     else db.table(data["simulated_weight_table"])
 )
 
-# db = TinyDB("../data/sim_db.json")
-# table = db.table("weight_training_log")
-
 
 @pytest.mark.parametrize(
     "test_input_split,test_input_exercise",
@@ -46,4 +43,42 @@ def test_one_rep_max_estimator():
     """Verify that 1RM has progression."""
     df = get_df(table, "legs", "squat")
     df_1rm = one_rep_max_estimator(df)
-    assert df_1rm.loc["2019-01-04", "1RM"] <= df_1rm.loc["2019-01-09", "1RM"]
+    # Check that for program_1, first squat 1RM is less or equal to 1RM of last squat
+    assert df_1rm.loc["2021-12-11", "1RM"] <= df_1rm.loc["2022-03-14", "1RM"]
+
+
+def test_get_data():
+    """Verify that the correct lists (timestamps and 1RM estimates) are returned,
+    for real training data from program_1
+    """
+    df = get_df(table, "legs", "squat")
+    df_1rm = one_rep_max_estimator(df)
+    x, y = get_data(df_1rm)
+
+    real_x_program_1 = [
+        1639177200.0,
+        1640386800.0,
+        1640991600.0,
+        1642201200.0,
+        1643238000.0,
+        1644015600.0,
+        1644620400.0,
+        1645830000.0,
+        1646434800.0,
+        1647212400.0,
+    ]
+    real_y_program_1 = [
+        102.86,
+        91.43,
+        102.86,
+        108.11,
+        108.11,
+        108.11,
+        108.11,
+        108.11,
+        114.29,
+        114.29,
+    ]
+
+    assert x[:10] == real_x_program_1
+    assert y[:10] == real_y_program_1
