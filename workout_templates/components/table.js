860461f1569c418592b5fef80ce55181d6afa80d
Gustav Rasmussen <gcr84@hotmail.com>
2022-05-24 22:10:19 +0200

    chore: add reusable web components

---
+++ workout_templates/components/table.js
@@ -0,0 +1,133 @@
+class ThreeSetTable extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+  connectedCallback() {
+    this.innerHTML = `
+      <style>
+
+      .exercise_name {
+        display: grid;
+        place-items: center;
+      }
+
+      table.GeneratedTable {
+        width: 100%;
+        background-color: #ffffff;
+        border-collapse: collapse;
+        border-width: 2px;
+        border-color: #002fff;
+        border-style: solid;
+        color: #000000;
+      }
+
+      table.GeneratedTable td,
+      table.GeneratedTable th {
+        border-width: 1px;
+        border-color: #003cff;
+        border-style: solid;
+        padding: 3px;
+      }
+
+      table.GeneratedTable thead {
+        background-color: #009dff;
+      }
+
+      </style>
+
+        <table class="GeneratedTable">
+          <thead>
+            <tr>
+              <th>set no</th>
+              <th>reps</th>
+              <th>weight / kg</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="exercise_name"><strong>1</strong></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td class="exercise_name"><strong>2</strong></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td class="exercise_name"><strong>3</strong></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+    `;
+  }
+}
+
+class TwoSetTable extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+  connectedCallback() {
+    this.innerHTML = `
+      <style>
+
+      .exercise_name {
+        display: grid;
+        place-items: center;
+      }
+
+      table.GeneratedTable {
+        width: 100%;
+        background-color: #ffffff;
+        border-collapse: collapse;
+        border-width: 2px;
+        border-color: #002fff;
+        border-style: solid;
+        color: #000000;
+      }
+
+      table.GeneratedTable td,
+      table.GeneratedTable th {
+        border-width: 1px;
+        border-color: #003cff;
+        border-style: solid;
+        padding: 3px;
+      }
+
+      table.GeneratedTable thead {
+        background-color: #009dff;
+      }
+
+      </style>
+
+        <table class="GeneratedTable">
+          <thead>
+            <tr>
+              <th>set no</th>
+              <th>reps</th>
+              <th>weight / kg</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td class="exercise_name"><strong>1</strong></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td class="exercise_name"><strong>2</strong></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+    `;
+  }
+}
+
+customElements.define('three-set-table-component', ThreeSetTable);
+customElements.define('two-set-table-component', TwoSetTable);
