c76d77f31fe0c58724ec7cff5d27d75b7d91968e
Gustav Rasmussen <gcr84@hotmail.com>
2022-07-16 16:07:34 +0200

    chore: complete type hints and add docstrings

--- src/helpers/cleanup.py
+++ src/helpers/cleanup.py
@@ -1,7 +1,7 @@
 """
 Date: 2021-12-25
 Author: Gustav Collin Rasmussen
-Purpose: empty specified folder
+Purpose: empty specified folder or file
 """
 
 import os
@@ -9,25 +9,40 @@ import pathlib
 import sys
 
 
-def cleanup(path) -> None:
-    """Empty all files in path."""
+def cleanup(path: str) -> None:
+    """Empty all files in path.
+
+    :param path: _description_
+    :type path: str
+    """
     p = pathlib.Path(path)
     all_files = os.listdir(p)
     for f in all_files:
         os.remove(p / f)
 
 
-def empty_file(file_path) -> None:
-    """Empty content in file."""
+def empty_file(file_path: str) -> None:
+    """Empty content in file.
+
+    :param file_path: _description_
+    :type file_path: str
+    """
     open(file_path, "w").close()
 
 
-def main(path, file_path):
+def main(path: str, file_path: str) -> None:
+    """_summary_
+
+    :param path: _description_
+    :type path: str
+    :param file_path: _description_
+    :type file_path: str
+    """
     cleanup(path)
     empty_file(file_path)
 
 
 if __name__ == "__main__":
-    path = sys.argv[1]
-    file_path = sys.argv[2]
+    path: str = sys.argv[1]
+    file_path: str = sys.argv[2]
     main(path, file_path)
