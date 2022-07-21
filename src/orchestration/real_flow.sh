b0108ef5058382a67267089b5b5c21b8881382dc
Gustav Rasmussen <gcr84@hotmail.com>
2022-07-21 14:41:53 +0200

    chore: add today's workout to db

--- src/orchestration/real_flow.sh
+++ src/orchestration/real_flow.sh
@@ -4,11 +4,11 @@
 # Author: Gustav Collin Rasmussen
 # Purpose: BASH workflow that inserts data in database.
 
-python3 src/CRUD/insert.py --datatype real --dates 2022-07-20  # 2022-03-02,2022-03-03  # --workout_number 2
+python3 src/CRUD/insert.py --datatype real --dates 2022-07-21  # 2022-03-02,2022-03-03  # --workout_number 2
 echo 'data inserted in database. Preparing figures ..'
 python3 src/model/plot_model.py --datatype real --pgm gvt
-# open img/real_fitted_data_squat_gvt.png
-open img/real_fitted_data_barbell_bench_press_gvt.png
+open img/real_fitted_data_squat_gvt.png
+# open img/real_fitted_data_barbell_bench_press_gvt.png
 # open img/real_fitted_data_squat_splines.png
 # open img/real_fitted_data_deadlift_splines.png
 # open img/real_fitted_data_seated_row_splines.png
